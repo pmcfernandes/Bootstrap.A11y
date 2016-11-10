99980b9681f94c28d5ded49e9d52d5a8c830e7bf
Pedro Fernandes <me@pfernandes.pt>
2016-11-10 16:57:16 +0000

    Removed Using Lines

--- Tie.Controls.Bootstrap/Button.cs
+++ Tie.Controls.Bootstrap/Button.cs
@@ -16,7 +16,6 @@ using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Tie.Controls.Bootstrap
 {
--- Tie.Controls.Bootstrap/TabControl.cs
+++ Tie.Controls.Bootstrap/TabControl.cs
@@ -13,10 +13,8 @@
 // Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Security.Permissions;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
